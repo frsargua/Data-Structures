2f4d1758174797b653aa6d3d678ee6e0271570e6
frsargua <frsargua@gmail.com>
2022-08-11 10:56:07 +0100

    binary search tree algorithm

--- stacks/index.js
+++ stacks/index.js
@@ -1,66 +1,225 @@
+const e = require("express");
 const express = require("express");
 const PORT = 3001;
 const app = express();
 app.get("/", (req, res) => res.send("<h1>Started</h1>"));
 
-// Stacks
-//A stack contains: push, pop, peek, length functions
-
-//Basic functionality of stack
-let letters = [];
-
-let word = "racecar";
-var reverseWord = "";
-
-for (let i = 0; i < word.length; i++) {
-  letters.push(word[i]);
+//Binary Search Tree can only have two branches at a single node unlike a normal tree
+//each insertion deletion and look up takes time proportional to the logarithm of the number of items stored in the tree
+//Slower than a hash table
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
 }
-for (let i = 0; i < word.length; i++) {
-  reverseWord += letters.pop();
-}
-if (reverseWord === word) console.log(word + " is a palindrome");
-else console.log(word + "  is not a palindrome");
 
-//Stack object
-let Stack = function () {
-  this.count = 0;
-  this.storage = [];
-
-  //   Adds a value onto the end of the stack
-  this.push = function (value) {
-    this.storage[this.count] = value;
-    this.count++;
-  };
-
-  //Removes and returns the value at the end of the stack
-  this.pop = function () {
-    if (this.count == 0) {
-      return undefined;
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
     }
+  }
 
-    this.count--;
-    let results = this.storage[this.count];
-    delete this.storage[this.count];
-    return results;
-  };
+  findMin() {
+    let current = this.root;
+    while (current.left !== null) {
+      current = current.left;
+    }
+    return current.data;
+  }
 
-  this.size = function () {
-    return this.count;
-  };
+  findMax() {
+    let current = this.root;
+    while (current.right !== null) {
+      current = current.right;
+    }
+    return current.data;
+  }
 
-  //   Retuns the value at the end of the stack
-  this.peek = function () {
-    return this.storage[this.count - 1];
-  };
-};
+  find(data) {
+    let current = this.root;
+    while (current.data !== data) {
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+      if (current == null) {
+        return null;
+      }
+    }
+    return current;
+  }
 
-let myStack = new Stack();
+  isPresent(data) {
+    let current = this.root;
+    while (current) {
+      if (data === current.data) {
+        return true;
+      }
+      if (data < current.data) {
+        current = current.left;
+      } else {
+        current = current.right;
+      }
+    }
+    return false;
+  }
+  remove(data) {
+    const removeNode = function (node, data) {
+      if (node == null) {
+        return null;
+      }
+      if (data == node.data) {
+        // node has no children
+        if (node.left == null && node.right == null) {
+          return null;
+        }
+        // node has no left child
+        if (node.left == null) {
+          return node.right;
+        }
+        // node has no right child
+        if (node.right == null) {
+          return node.left;
+        }
+        // node has two children
+        var tempNode = node.right;
+        while (tempNode.left !== null) {
+          tempNode = tempNode.left;
+        }
+        node.data = tempNode.data;
+        node.right = removeNode(node.right, tempNode.data);
+        return node;
+      } else if (data < node.data) {
+        node.left = removeNode(node.left, data);
+        return node;
+      } else {
+        node.right = removeNode(node.right, data);
+        return node;
+      }
+    };
+    this.root = removeNode(this.root, data);
+  }
+  isBalanced() {
+    return this.findMinHeight() >= this.findMaxHeight() - 1;
+  }
+  findMinHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMinHeight(node.left);
+    let right = this.findMinHeight(node.right);
+    if (left < right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  findMaxHeight(node = this.root) {
+    if (node == null) {
+      return -1;
+    }
+    let left = this.findMaxHeight(node.left);
+    let right = this.findMaxHeight(node.right);
+    if (left > right) {
+      return left + 1;
+    } else {
+      return right + 1;
+    }
+  }
+  inOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traverseInOrder(node) {
+        node.left && traverseInOrder(node.left);
+        result.push(node.data);
+        node.right && traverseInOrder(node.right);
+      }
+      traverseInOrder(this.root);
+      return result;
+    }
+  }
+  preOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePreOrder(node) {
+        result.push(node.data);
+        node.left && traversePreOrder(node.left);
+        node.right && traversePreOrder(node.right);
+      }
+      traversePreOrder(this.root);
+      return result;
+    }
+  }
+  postOrder() {
+    if (this.root == null) {
+      return null;
+    } else {
+      var result = new Array();
+      function traversePostOrder(node) {
+        node.left && traversePostOrder(node.left);
+        node.right && traversePostOrder(node.right);
+        result.push(node.data);
+      }
+      traversePostOrder(this.root);
+      return result;
+    }
+  }
 
-myStack.push(1);
-myStack.push(2);
-console.log(myStack.peek());
-console.log(myStack.pop());
-console.log(myStack.peek());
+  levelOrder() {
+    let result = [];
+    let Q = [];
+    if (this.root != null) {
+      Q.push(this.root);
+      while (Q.length > 0) {
+        let node = Q.shift();
+        result.push(node.data);
+        if (node.left != null) {
+          Q.push(node.left);
+        }
+        if (node.right != null) {
+          Q.push(node.right);
+        }
+      }
+      return result;
+    } else {
+      return null;
+    }
+  }
+}
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
